f81e223a9dea42f75218fe87f1c643d59c0bea19
Shriti Negi <shritinegi24@gmail.com>
2025-12-29 00:03:00 +0530

    fix storage.js

--- frontend/mindLog/src/services/storage.js
+++ frontend/mindLog/src/services/storage.js
@@ -1,24 +1,39 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage"
+const STORAGE_KEY='@mindlog_entries'
 
-const STORAGE_KEY = '@journal_entries';
-
-// Load all entries
-export const getEntries = async () => {
-  try {
-    const jsonValue = await AsyncStorage.getItem(STORAGE_KEY);
-    return jsonValue != null ? JSON.parse(jsonValue) : [];
-  } catch (e) {
-    console.error("Error reading entries:", e);
-    return [];
+export const getEntries= async () => {
+    try {
+        const data=await AsyncStorage.getItem(STORAGE_KEY)
+        return data ? JSON.parse(data) : []
+    } catch (err){
+        console.log('error fetching entries',err)
+        return []
+    }
   }
-};
 
-// Save the entire list of entries
-export const saveEntries = async (entries) => {
-  try {
-    const jsonValue = JSON.stringify(entries);
-    await AsyncStorage.setItem(STORAGE_KEY, jsonValue);
-  } catch (e) {
-    console.error("Error saving entries:", e);
+  const saveAllEntries=async (entries) => {
+    try {
+    const data=JSON.stringify(entries);
+    await AsyncStorage.setItem(STORAGE_KEY, data)
+  } catch (err) {
+    console.error("Error saving entries:", err)
   }
-};
+}
+
+export const addEntry=async (newEntry) => {
+  try {
+    const currentEntries=await getEntries();
+    const entryWithMeta = {
+      id: Date.now().toString(),
+      timestamp: new Date().toISOString(),
+      ...newEntry 
+    }
+
+    const updatedEntries=[...currentEntries, entryWithMeta]
+    
+    await saveAllEntries(updatedEntries)
+    return updatedEntries
+  } catch (err) {
+    console.error("Failed to add entry:", err)
+  }
+}
